053feb23fdbe1a9cfa0e552beaa053a7bbdc976e
张松 <s.zhang.cedar@gmail.com>
2025-02-28 00:48:57 +0800

    1

--- pomodoro.py
+++ pomodoro.py
@@ -1,20 +1,23 @@
 import rumps
 
+
 class PomodoroApp(object):
     def __init__(self):
+        # 配置项中文化修改
         self.config = {
-            "app_name": "Pomodoro",
-            "start": "Start Timer",
-            "pause": "Pause Timer",
-            "continue": "Continue Timer",
-            "stop": "Stop Timer",
-            "break_message": "Time is up! Take a break :)",
-            "interval": 1500  # 25分钟，单位为秒
+            "app_name": "番茄钟",
+            "start": "开始计时",
+            "pause": "暂停",
+            "continue": "继续",
+            "stop": "停止",
+            "break_message": "时间到！该休息一下了 :)",
+            "interval": 30  # 25分钟，单位为秒
         }
         self.app = rumps.App(self.config["app_name"])
         self.timer = rumps.Timer(self.on_tick, 1)
         self.interval = self.config["interval"]
         self.set_up_menu()
+        # 菜单项使用中文配置
         self.start_pause_button = rumps.MenuItem(title=self.config["start"], callback=self.start_timer)
         self.stop_button = rumps.MenuItem(title=self.config["stop"], callback=None)
         self.app.menu = [self.start_pause_button, self.stop_button]
@@ -29,7 +32,10 @@ class PomodoroApp(object):
         mins = time_left // 60 if time_left >= 0 else time_left // 60 + 1
         secs = time_left % 60 if time_left >= 0 else (-1 * time_left) % 60
         if mins == 0 and time_left < 0:
-            rumps.notification(title=self.config["app_name"], subtitle=self.config["break_message"], message='')
+            rumps.notification(
+                title=self.config["app_name"],
+                subtitle=self.config["break_message"],  # 中文提示
+                message='')
             self.stop_timer()
             self.stop_button.set_callback(None)
         else:
@@ -38,20 +44,21 @@ class PomodoroApp(object):
         sender.count += 1
 
     def start_timer(self, sender):
-        if sender.title.lower().startswith(("start", "continue")):
+        # 修改判断逻辑适配中文
+        if sender.title in [self.config["start"], self.config["continue"]]:
             if sender.title == self.config["start"]:
                 self.timer.count = 0
                 self.timer.end = self.interval
-            sender.title = self.config["pause"]
+            sender.title = self.config["pause"]  # 显示"暂停"
             self.timer.start()
         else:
-            sender.title = self.config["continue"]
+            sender.title = self.config["continue"]  # 显示"继续"
             self.timer.stop()
 
-    def stop_timer(self):
+    def stop_timer(self, _=None):
         self.set_up_menu()
         self.stop_button.set_callback(None)
-        self.start_pause_button.title = self.config["start"]
+        self.start_pause_button.title = self.config["start"]  # 重置为"开始计时"
 
     def run(self):
         self.app.run()
